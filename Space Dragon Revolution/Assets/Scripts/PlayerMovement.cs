bf4223b45d9051a7099e584cc2379d9752bb6ac9
CastleBomber <richie.ucsb@gmail.com>
2021-01-13 12:40:50 -0800

    DebuggingJump

--- Space Dragon Revolution/Assets/Scripts/PlayerMovement.cs
+++ Space Dragon Revolution/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
     public float speed = 12f;
     public float gravity = -9.81f;
+    public float jumpHeight = 3f;
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
@@ -28,6 +29,12 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+		{
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+		}
+
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
